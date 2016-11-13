364e6e3f1848f6af3a230838a943c0e36d99637c
syasuker <syasuker@qq.com>
2016-11-13 22:40:14 +0800

    1

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /.tomcat_admin_created ]; then
+    /create_tomcat_admin_user.sh
+fi
+
+if [ ! -f /.cpWar]; then
+    /cpWar.sh
+fi
+
+exec ${CATALINA_HOME}/bin/catalina.sh run
